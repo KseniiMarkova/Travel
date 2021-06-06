fcf5297ad68e7f311546381a1bc5f4379ef92a47
Ksenii Markova <kdmp4121@gmail.com>
2021-06-07 01:31:47 +0300

    Optimized images, js, html and css files.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import '../css/style.css';
 (function () {
 	/*вернет первый элемент с заданным селектором*/
 	const header = document.querySelector('.header');
@@ -15,11 +16,11 @@
 }());/*самовызывающаяся функция, которая отработает как только распарсится js*/
 
 // Burger handler
-
 (function () {
 	const burgerItem = document.querySelector('.burger');
 	const menu = document.querySelector('.header__nav');
 	const menuCloseItem = document.querySelector('.header__nav-close');
+	const menuLinks = document.querySelectorAll('.header__link');
 	// обработчик события открыть меню('событие которое хотим прослушивать')
 	burgerItem.addEventListener ('click', () => {
 		menu.classList.add('header__nav_active');
@@ -28,4 +29,53 @@
 	menuCloseItem.addEventListener('click', () => {
 		menu.classList.remove('header__nav_active');
 	});
+	if (window.innerWidth < 768) { //для перехода по частям страницы
+		for (let i = 0; i < menuLinks.length; i++) { //можно использовать forEach
+			menuLinks[i].addEventListener('click', () => {
+				menu.classList.remove('header__nav_active');
+			});
+		};
+	};
+}());
+
+//Scroll to anchors(плавная анимация скролла)
+(function () {
+	const smoothScroll = function (targetEl, duration) {
+		const headerElHeight = document.querySelector('.header').clientHeight; //получаем высоту хедера
+		let target = document.querySelector(targetEl);
+		let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+		let startPosition = window.pageYOffset;
+		let startTime = null;
+
+		//функция обработчик скролла (функция is)
+		const ease = function(t, b, c, d) {
+			t /= d / 2;
+			if (t < 1) return c / 2 * t * t + b;
+			t--;
+			return -c / 2 * (t * (t - 2) - 1) + b;
+		};
+
+		//функция анимации
+		const animation = function(currentTime){
+			if (startTime === null) startTime = currentTime;
+			const timeElapsed = currentTime - startTime;
+			const run = ease(timeElapsed, startPosition, targetPosition, duration);
+			window.scrollTo(0,run);
+			if (timeElapsed < duration) requestAnimationFrame(animation);
+		};
+		requestAnimationFrame(animation);
+
+	};
+
+	//подвешивание обработчика события на ссылки для срабатывания скрола smoothscroll
+	const scrollTo = function () {
+		const links = document.querySelectorAll('.js-scroll');
+		links.forEach(each => {
+			each.addEventListener('click', function () {
+				const currentTarget = this.getAttribute('href');
+				smoothScroll(currentTarget, 1000);
+			});
+		});
+	};
+	scrollTo();
 }());
